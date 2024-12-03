05f1586ed00941626e8a6eaae181039f43889469
koke2511 <j.d.ferrero@hotmail.com>
2024-12-03 13:15:44 +0100

    nuevos cambios

---
+++ utils.ts
@@ -0,0 +1,35 @@
+import { Vehicle, VehicleModel } from "./types.ts";
+import { PartsModel, Parts } from "./types.ts";
+import { ObjectId } from "mongodb";
+
+const ObjectIdToString = (ids: ObjectId[]): string [] => ids.map(id => id.toString());
+
+export const fromModelToVehicle = (vehicleModel : VehicleModel): Vehicle => {
+    return{
+        id: vehicleModel._id!.toString(),
+        name: vehicleModel.name,
+        manufactue: vehicleModel.manufacture,
+        year: vehicleModel.year,
+        parts: vehicleModel.parts.map(u=> ({
+            id: u.toString(),
+            name: "", 
+            price: 0, 
+            vehicleId: []
+        })),
+    };
+};
+
+export const fromModelToParts = (partsModel : PartsModel) : Parts => {
+    return{
+        id: partsModel._id!.toString(),
+        name: partsModel.name, 
+        price: partsModel.price,
+        vehicleId: partsModel.vehicleId.map(u => ({
+            id: u.toString(),
+            name: "",
+            manufactue: "",
+            year: 0,
+            parts: []
+        })),
+    };
+};
