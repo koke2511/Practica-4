976ce4d3b9add2cda1fb9ea02422bbdec0dc1d60
koke2511 <j.d.ferrero@hotmail.com>
2024-12-06 18:42:42 +0100

    nueva actualizacion

--- schema.ts
+++ schema.ts
@@ -5,7 +5,17 @@ type Vehicle {
     name: String!,
     manufactue: String!,
     year: Int!,
+    joke: String!,
     parts : [Parts!]!
+}
+
+type Query {
+    vehicles : [Vehicle!]!
+    vehicle (id: ID!) : Vehicle
+    parts : [Parts!]!
+    vehicleByManufacturer(manufactue: String!) : [Vehicle!]!
+    partsByVehicle(vehicleId: ID!) : [Parts!]!
+    vehicleByYearRange(startYear: Int!, endYear: Int!) : [Vehicle!]!
 }
 
 
